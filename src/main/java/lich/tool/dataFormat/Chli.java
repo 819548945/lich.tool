4fb076ac3127b2b41af39ae9a6b34340e41808e9
819548945 <819548945@qq.com>
2020-04-26 15:04:13 +0800

    chli init
    data Formant chli

---
+++ src/main/java/lich/tool/dataFormat/Chli.java
@@ -0,0 +1,112 @@
+package lich.tool.dataFormat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+/**
+ * Simple data encapsulation format
+ * @author liuch
+ */
+public  class Chli{
+	 private List<byte[]> decodeData=new ArrayList<byte[]>();
+	/**
+	 * clear Chli data
+	 * @return oneself
+	 */
+	 public Chli clear() {
+		 decodeData.clear();
+		 return this;
+	 }
+	 /**
+	  * reLoad data
+	  * @param data encData
+	  * @return oneself
+	  */
+	 public Chli reLoadData(List<byte[]> data) {
+		 if(data==null) {
+			 throw new NullPointerException("data is null");
+		 }
+		 decodeData=data;
+		 return this;
+	 }
+	 /**
+	  * add data
+	  * @param data encData
+	  * @return oneself
+	  */
+	 public Chli addData(byte[] b){
+		 decodeData.add(b);
+		 return this;
+	 }
+	 /**
+	  * add data by index
+	  * @param index index
+	  * @param b encData
+	  * @return oneself
+	  */
+	 public Chli addData(int index,byte[] b){
+		 decodeData.add(index,b);
+		 return this;
+	 }
+	 /**
+	  * add datas
+	  * @param c encDatas
+	  * @return oneself
+	  */
+	 public Chli addDatas(Collection<byte[]> c){
+		 decodeData.addAll(c);
+		 return this;
+	 }
+	 /**
+	  * Get pending data
+	  * @return pending data
+	  */
+	 public List<byte[]> getDatas(){
+		 return decodeData;
+	 }
+	 /**
+	  * to enc Version 1 data
+	  * @return enc data
+	  */
+	 public byte[] encodeV1() {
+		 int size=3;
+		 for(int i=0;i<decodeData.size();i++) {
+			 size=size+decodeData.get(i).length+1;
+		 }
+		 byte [] b=new byte[size];
+		 b[0]=(byte)0x0A;
+		 b[1]=(byte)0xF9;
+		 b[2]=(byte)0x01;
+		 for(int i=0,j=3;i<decodeData.size();i++) {
+			 b[j]=(byte)decodeData.get(i).length;
+			 System.arraycopy(decodeData.get(i), 0, b, j+1, decodeData.get(i).length);
+			 j=j+1+decodeData.get(i).length;
+		 }
+		 
+		 return b;
+	 }
+	 /**
+	  * decode enc data
+	  * @param encodeData enc data
+	  * @return
+	  */
+	 public Chli decode(byte[] encodeData) { 
+		 if(!((encodeData[0]==(byte)0x0A)&&(encodeData[1]==(byte)0xF9))) {
+			 throw new NumberFormatException("format not chli");
+		 }
+		 if(encodeData[2]==(byte)0x01){
+			 decodeData.clear();
+			 for(int i=3;i<encodeData.length;i++) {
+				 byte [] b=new byte[encodeData[i]];
+				 System.arraycopy(encodeData, i+1, b, 0, encodeData[i]);
+				 decodeData.add(b);
+				 i=i+encodeData[i];
+				 
+			 }
+		 }else {
+			 throw new NumberFormatException("version is not definition");
+		 }
+		 return this;
+	 }
+	 
+ }
